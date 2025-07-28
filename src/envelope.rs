5a4d9cc87d18bd6d9140dc9955a98eaf87022410
Loev06 <105797818+Loev06@users.noreply.github.com>
2025-07-28 22:54:49 +0200

    Basic ADSR envelope

---
+++ src/envelope.rs
@@ -0,0 +1,95 @@
+#[derive(PartialEq)]
+enum EnvelopeState {
+    Idle,
+    TriggeredOn(f32),  // Time when the envelope was triggered on
+    TriggeredOff(f32), // Time when the envelope was triggered off
+}
+
+pub struct EnvelopeADSR {
+    attack_time: f32,
+    decay_time: f32,
+    release_time: f32,
+
+    attack_amplitude: f32,
+    sustain_amplitude: f32,
+
+    state: EnvelopeState,
+}
+
+impl EnvelopeADSR {
+    pub fn new(
+        attack_time: f32,
+        decay_time: f32,
+        release_time: f32,
+        attack_amplitude: f32,
+        sustain_amplitude: f32,
+    ) -> Self {
+        EnvelopeADSR {
+            attack_time,
+            decay_time,
+            release_time,
+            attack_amplitude,
+            sustain_amplitude,
+            state: EnvelopeState::Idle,
+        }
+    }
+
+    pub fn is_idle(&self) -> bool {
+        self.state == EnvelopeState::Idle
+    }
+
+    pub fn trigger_on(&mut self, current_time: f32) {
+        self.state = EnvelopeState::TriggeredOn(current_time);
+    }
+
+    pub fn trigger_off(&mut self, current_time: f32) {
+        self.state = EnvelopeState::TriggeredOff(current_time);
+    }
+
+    pub fn get_amplitude(&mut self, current_time: f32) -> f32 {
+        if let EnvelopeState::TriggeredOff(time_off) = self.state {
+            let elapsed = current_time - time_off;
+
+            if elapsed >= self.release_time {
+                self.state = EnvelopeState::Idle;
+                return 0.0;
+            }
+        }
+
+        let amplitude = self.get_raw_amplitude(current_time);
+
+        if amplitude < 0.0001 {
+            0.0
+        } else {
+            amplitude
+        }
+    }
+
+    fn get_raw_amplitude(&self, current_time: f32) -> f32 {
+        match self.state {
+            EnvelopeState::Idle => 0.0,
+            EnvelopeState::TriggeredOn(time_on) => {
+                let elapsed = current_time - time_on;
+
+                if elapsed <= self.attack_time {
+                    // Attack phase
+                    elapsed / self.attack_time * self.attack_amplitude
+                } else if elapsed <= self.attack_time + self.decay_time {
+                    // Decay phase
+                    self.attack_amplitude
+                        + (elapsed - self.attack_time) / self.decay_time
+                            * (self.sustain_amplitude - self.attack_amplitude)
+                } else {
+                    // Sustain phase
+                    self.sustain_amplitude
+                }
+            }
+            EnvelopeState::TriggeredOff(time_off) => {
+                // Release phase
+                let elapsed = current_time - time_off;
+
+                (1.0 - elapsed / self.release_time) * self.sustain_amplitude
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod audio_device;
+mod envelope;
 mod source;
 mod types;
 
 use anyhow::Result;
 use cpal::traits::StreamTrait;
 use crossbeam::channel;
+use envelope::EnvelopeADSR;
 use source::{
     noise::Noise,
     oscillator::{Oscillator, Waveform},
@@ -16,11 +18,20 @@ fn main() -> Result<()> {
     let (stream, sample_rate) = audio_device::stream_setup(r)?;
     stream.play()?;
 
-    let mut oscillator =
-        // Oscillator::new(Waveform::SineSawtooth(10), 110.0, 0.03, 0.0, sample_rate.0);
-        Noise::new(0.03);
-    for _ in 0..(sample_rate.0 * 2) {
-        let sample = oscillator.next_sample();
+    let mut oscillator = Oscillator::new(Waveform::Square, 440.0, 0.02, 0.0, sample_rate.0);
+    // Noise::new(0.03);
+    let mut envelope = EnvelopeADSR::new(0.3, 0.3, 0.4, 1.0, 0.6);
+
+    for i in 0..(sample_rate.0 * 2) {
+        let t = i as f32 / sample_rate.0 as f32;
+
+        match i {
+            0 => envelope.trigger_on(t),
+            50000 => envelope.trigger_off(t),
+            _ => (),
+        }
+
+        let sample = oscillator.next_sample() * envelope.get_amplitude(t);
         s.send(types::Message(sample, sample))?;
     }
 
